00dc9c7cad8d5b60b7a5e20046868fa3c302fbdf
aiue01212 <aiue85605@gmail.com>
2025-10-30 17:49:02 +0900

    2025-10-30 指摘事項修正

--- loginapp/src/main/java/com/example/loginapp/dto/ErrorResponse.java
+++ loginapp/src/main/java/com/example/loginapp/dto/ErrorResponse.java
@@ -2,14 +2,12 @@ package com.example.loginapp.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * エラー時のレスポンスを返すDTOクラス
  */
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class ErrorResponse implements SessionCheckResponse {
 
     /** エラーメッセージ */
